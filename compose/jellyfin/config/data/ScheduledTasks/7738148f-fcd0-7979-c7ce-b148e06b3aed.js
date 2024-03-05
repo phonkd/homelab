46a7efc64a8d55e065c3eaa906343fbc6a8cbb40
Phonkd <enst18.12@gmail.com>
2024-03-05 23:46:41 +0000

    no longer everything public with ip whitelist

---
+++ compose/jellyfin/config/data/ScheduledTasks/7738148f-fcd0-7979-c7ce-b148e06b3aed.js
@@ -0,0 +1,1 @@
+{"StartTimeUtc":"2024-03-05T20:57:44.2671202Z","EndTimeUtc":"2024-03-05T20:58:11.4577994Z","Status":"Completed","Name":"Scan Media Library","Key":"RefreshLibrary","Id":"7738148ffcd07979c7ceb148e06b3aed"}
---
+++ compose/jellyfin/config/data/ScheduledTasks/f9b057c0-54e9-e6da-ee4a-88ffd146a403.js
@@ -0,0 +1,1 @@
+{"StartTimeUtc":"2024-03-05T23:25:30.6675283Z","EndTimeUtc":"2024-03-05T23:25:31.3180798Z","Status":"Completed","Name":"Update Plugins","Key":"PluginUpdates","Id":"f9b057c054e9e6daee4a88ffd146a403"}
